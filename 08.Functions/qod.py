65fb641b8f44c37fb76aefb518b99282b6a8ca8c
giridher sadineni <giridher@hotmail.com>
2019-08-14 18:35:04 +0530

    Improved quotes Application

--- 08.Functions/qod.py
+++ 08.Functions/qod.py
@@ -1,26 +1,43 @@
+# coding=utf-8
 import random as r
-quotes=[]
-quotes.append('“The Way Get Started Is To Quit Talking And Begin Doing.” – Walt Disney')
-quotes.append('“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.” – Winston Churchill')
-
+quotes={}
 def quoteplease():
-    quote=r.choice(quotes)
-    print(quote)
+    print(quotes)
     return 
 
 def addquote():
-    quote=input("Enter a Quote")
-    quotes.append(quote)
+    noquotes=[]
+    quotedby=input("Enter the name of person quoted it")
+    quoteslist=quotes.get(quotedby,noquotes)
+    quote=input("enter the quote")
+    quoteslist.append(quote)
+    quotes[quotedby]=quoteslist
 
+def searchbyperson():
+    person=input("enter person name")
+    if(person in quotes.keys()):
+        print("Quotes of ",person)
+        for quote in quotes.get(person):
+            print(quote)
+    else:
+        print("No Quotes. Please add if you know one")
 
+def randomquote():
+    quote=r.choice(quotes.get(r.choice(quotes.keys())))
+    print(quote)
+    
 functions={
     1:addquote,
-    2:quoteplease
+    2:searchbyperson,
+    3:quoteplease,
+    4:randomquote
 }
 while 1:
     print("Welcome to the Skillverse Quote of the Day")
-    print("Enter 1 to Submit a Quote")
-    print("Enter 2 to view a random Quote")
+    print("1. Submit a Quote")
+    print("2. Browse Quotes by Person")
+    print("3. Display all quotes")
+    print("4. Show a random Quote")
     choice=int(input())
     task=functions.get(choice,"Cannot Do that!")
     task()
