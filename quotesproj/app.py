8e36c1a5704e3b96427964dae2068195422d0f2a
giridherSadineni <giridher@hotmail.com>
2019-09-19 17:17:08 +0530

    done

--- quotesproj/app.py
+++ quotesproj/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, escape, request,render_template
-
+import mysql.connector
 app = Flask(__name__)
+
 
 @app.route('/')
 def mainpage():
@@ -21,12 +22,40 @@ def createcategory():
 def viewcategory():
     return render_template("category/view.html")
 
+@app.route('/storecategory',methods=['POST'])
+def storecategory():
+    category=request.form.get('category')
+    description=request.form.get('description')
+    try:
+        cnx=mysql.connector.connect(user="root",host="localhost",password="skillverse",database="quotes")
+        cur=cnx.cursor()
+        query="insert into categories(category name,description) values('"+category+"','"+description+"')"
+        cur.execute(query)
+        cur.close()
+        cnx.commit()
+    except mysql.connector.Error as e:
+        print(e)
+
+    return render_template('categories/index.html')
+
 
 
 #quotes routes
 @app.route('/quotes')
 def quotes():
-    return render_template("quotes/index.html")
+    try:
+        cnx=mysql.connector.connect(user="root",host="localhost",password="skillverse",database="quotes")
+        cur=cnx.cursor()
+        query="select * from quote"
+        cur.execute(query)
+        quotes=cur.fetchall()
+        print(quotes)
+        cur.close()
+        cnx.commit()
+    except mysql.connector.Error as e:
+        print(e)
+
+    return render_template("quotes/index.html",quotes=quotes)
 
 @app.route('/quotes/create')
 def createquote():
@@ -36,4 +65,51 @@ def createquote():
 def viewquote():
     return render_template("quotes/view.html")
 
+@app.route('/storequote',methods=['POST'])
+def storequote():
+    quote=request.form.get('quote')
+    author=request.form.get('author')
+    category=request.form.get('category')
+    keyword=request.form.get('keyword')
+    try:
+        cnx=mysql.connector.connect(user="root",host="localhost",password="skillverse",database="quotes")
+        cur=cnx.cursor()
+        query="insert into quote(quote,author,category,keyword) values('"+quote+"','"+author+"','"+category+"','"+keyword+"')"
+        cur.execute(query)
+        cur.close()
+        cnx.commit()
+    except mysql.connector.Error as e:
+        print(e)
 
+    return render_template('quotes/index.html')
+
+
+#keywords routes
+@app.route('/keywords')
+def keywords():
+    return render_template("keywords/index.html")
+
+@app.route('/keywords/create')
+def createkeyword():
+    return render_template("keywords/create.html")
+
+@app.route('/storekeyword',methods=['POST'])
+def storekeyword():
+    keyword=request.form.get('keyword')
+
+    print(keyword)
+    try:
+        cnx=mysql.connector.connect(user="root",host="localhost",password="skillverse",database="quotes")
+        cur=cnx.cursor()
+        query="insert into keywords(keyword,category) values('"+keyword+"','"+category+"')"
+        cur.execute(query)
+        cur.close()
+        cnx.commit()
+    except mysql.connector.Error as e:
+        print(e)
+
+    return render_template('keywords/index.html')
+
+@app.route('/keywords/view')
+def viewkeyword():
+    return render_template("keywords/view.html")
